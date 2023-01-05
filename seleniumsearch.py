bd713c40cbbab1a7047a3eb001cc49b5c1d099a9
gwiggen1 <121186555+gwiggen1@users.noreply.github.com>
2023-01-05 11:49:09 -0500

    Update seleniumsearch.py

--- seleniumsearch.py
+++ seleniumsearch.py
@@ -8,6 +8,9 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
+#Search Input
+Search = input('What Should I Search?  ')
+
 #Unlocks the browser from code
 chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
@@ -16,10 +19,10 @@ chrome_options.add_experimental_option("detach", True)
 wbrowser = webdriver.Chrome(chrome_options=chrome_options)
 
 #Opens Google in web browser
-wbrowser.get("https://google.com")
+wbrowser.get("https://google.com") #Can switch out with Bing or Other Search Engine Url bc field name is also 'q'
 wbrowser.maximize_window()
 
 #Finds search bar, Enters Bing in search bar, Submits
 input_Element = wbrowser.find_element(By.NAME,'q')
-input_Element.send_keys('Bing')
+input_Element.send_keys(Search)
 input_Element.submit()
